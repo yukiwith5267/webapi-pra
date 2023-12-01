6d7a7303e3fda3f4d2a16fd168e0b04f82938fe6
手塚裕貴 <tedzukayuki@macbook-pro-6.local>
2023-12-02 07:59:01 +0900

    app.jsを作成

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+let booklog = {};
 
 app.use(express.json());
 
-app.post("/", (req, res) => {
+// /booklogにPOSTリクエストが来たら、リクエストボディをbooklogに代入
+app.post("/booklog", (req, res) => {
+  booklog = req.body;
+
+  if (!(booklog.name && booklog.text)) {
+    // nameとtextが両方なかった場合true->errorを返し終了
+    return res.json({
+      ok: false,
+      errror: "invalid params",
+    });
+  }
+
   res.json({
     ok: true,
+    booklog: booklog,
+  });
+});
+
+// /booklogにGETリクエストが来たら、booklogを返す
+app.get("/booklog", (req, res) => {
+  res.json({
+    ok: true,
+    booklog: [booklog],
   });
 });
 
